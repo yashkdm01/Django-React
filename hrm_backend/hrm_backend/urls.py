84ef643d8c42f2b7899d8ee7e32c384e01a32c5f
yashkdm01 <yskadam2108@gmail.com>
2025-12-18 09:45:25 +0000

    redirect root url to /api/departments/

--- hrm_backend/hrm_backend/urls.py
+++ hrm_backend/hrm_backend/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 
 from departments.views import DepartmentViewSet
@@ -8,6 +9,7 @@ router = DefaultRouter()
 router.register(r'departments', DepartmentViewSet, basename='department')
 
 urlpatterns = [
+    path('', RedirectView.as_view(url = '/api/departments/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
