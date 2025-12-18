201c0b36fe7435e8e6da312919c61414a47c3846
yashkdm01 <yskadam2108@gmail.com>
2025-12-18 12:44:16 +0000

    Fix STATIC_ROOT setting for Render

--- hrm_roles_backend/hrm_roles_backend/settings.py
+++ hrm_roles_backend/hrm_roles_backend/settings.py
@@ -118,4 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC-ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
