7348c1a821a0615c02901d6e7d661b822fdd22fd
yashkdm01 <yskadam2108@gmail.com>
2025-12-19 06:19:00 +0000

    change environment settings in setting.py

--- task_mgt_backend/task_mgt_backend/wsgi.py
+++ task_mgt_backend/task_mgt_backend/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_mgt_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_mgt_backend.task_mgt_backend.settings')
+
 
 application = get_wsgi_application()
