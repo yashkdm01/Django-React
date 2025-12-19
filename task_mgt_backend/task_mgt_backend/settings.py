8e8e94b9714868f9a0720e8b305a96f836e595a8
yashkdm01 <yskadam2108@gmail.com>
2025-12-19 06:35:50 +0000

    change tasks to task_mgt everywhere

--- task_mgt_backend/task_mgt_backend/settings.py
+++ task_mgt_backend/task_mgt_backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tasks'
+    'task_mgt_backend.tasks'
 ]
 
 MIDDLEWARE = [
--- task_mgt_backend/tasks/apps.py
+++ task_mgt_backend/tasks/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class TasksConfig(AppConfig):
-    name = 'tasks'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'task_mgt_backend.tasks'  # must be full path
