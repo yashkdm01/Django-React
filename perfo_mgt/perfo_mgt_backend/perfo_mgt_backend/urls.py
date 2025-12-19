d3e6d601578ea8c8eea6e7b3eade46022046f656
Yash Kadam <160143421+yashkdm01@users.noreply.github.com>
2025-12-19 15:30:34 +0530

    Delete perfo_mgt directory

--- perfo_mgt/perfo_mgt_backend/perfo_mgt_backend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.views.generic import RedirectView
-
-urlpatterns = [
-    path("", RedirectView.as_view(pattern_name="manager_dashboard", permanent=False)),
-    path("admin/", admin.site.urls),
-    path("reviews/", include("reviews.urls")),
-]
